8725c7723f6e10c453bc904ac962e05d484d55b1
Navu4 <51900952+Navu4@users.noreply.github.com>
2021-08-16 13:49:50 +0530

    update

--- src/services/demoService.js
+++ src/services/demoService.js
@@ -1,7 +1,7 @@
 import instance from '../instance';
 export const demoServices = async () => {
   try {
-    const res = await instance.get('/');
+    const res = await instance.get('/', );
     return res;
   } catch (error) {
     console.log(error);
